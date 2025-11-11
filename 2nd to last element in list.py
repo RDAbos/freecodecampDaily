5097887674e5d9331b7e16cad5151cd9eb8eadda
RDAbos <ryancboswell@outlook.com>
2025-11-11 10:44:21 -0500

    new files

---
+++ 2nd to last element in list.py
@@ -0,0 +1,7 @@
+#Read an integer n, then read n integers separated by spaces.
+#Print the second largest number in the list.
+if __name__ == '__main__':
+    n =int(input())
+    list = list(map(int, input().split()))
+    list.sort()
+    print(list[-2])
