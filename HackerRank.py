dcdccd7c8a66f1b0969e4aea22dcae6511956fe9
RDAbos <ryancboswell@outlook.com>
2025-11-11 09:27:06 -0500

    HackerRank

---
+++ HackerRank.py
@@ -0,0 +1,12 @@
+
+
+# creating a loop from a number input, that prints out the square of the number
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0, n):
+        print (i*i)
+
+
+
+
+        
